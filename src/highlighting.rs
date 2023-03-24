0de14b1492ad5d758500bc64d60c070d42bfde4b
Ruslan Yun <leonemsolis@gmail.com>
2023-03-24 11:16:52 +0600

    add character coloring

--- src/highlighting.rs
+++ src/highlighting.rs
@@ -6,6 +6,7 @@ pub enum Type {
     Number,
     Match,
     String,
+    Character,
 }
 
 impl Type {
@@ -14,6 +15,7 @@ impl Type {
             Type::Number => Color::Rgb { r: 220, g: 163, b: 163 },
             Type::Match => Color::Rgb { r: 38, g: 139, b: 210 },
             Type::String => Color::Rgb { r: 211, g: 54, b: 130 },
+            Type::Character => Color::Rgb { r: 108, g: 113, b: 196 },
             _ => Color::Rgb { r: 255, g: 255, b: 255 },
         }
     }
