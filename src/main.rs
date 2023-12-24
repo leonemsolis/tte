ce4f56673dea6079523cd055835460be177fc879
Ruslan Yun <leonemsolis@gmail.com>
2023-12-24 11:08:12 +0600

    clear

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-mod document;
-mod editor;
-mod filetype;
-mod highlighting;
-mod row;
-mod terminal;
-
-pub use row::Row;
-pub use document::Document;
-pub use editor::Editor;
-pub use editor::SearchDirection;
-pub use filetype::FileType;
-pub use filetype::HighlightingOptions;
-pub use terminal::Terminal;
-pub use editor::Position;
-
-fn main() -> Result<(), std::io::Error> {
-    Editor::default().run()
+fn main() {
+    println!("Hello world...")
 }
